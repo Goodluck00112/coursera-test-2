c446f7a6c08f0a93ef671fbb621fa22ba99c77c1
Goodluck Nnadi <155274314+Goodluck00112@users.noreply.github.com>
2024-10-29 06:27:51 +0100

    Create ListArray.js
    The listing of for loop using Array

---
+++ ListArray.js
@@ -0,0 +1,9 @@
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length ; i++) {
+        console.log(i, arr[i])
+    }
+    
+}
+    var colors = ['reg', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+    listArrayItems(colors);
+    
